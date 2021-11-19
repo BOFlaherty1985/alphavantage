357ff5ee8c0104f2718d0304d2b40d9755ab5ba9
Benjamin OFlaherty <b_oflaherty@hotmail.com>
2021-11-19 14:52:27 +0000

    Add CompanyOverviewController to retrieve company data from AlphaVantage

---
+++ src/main/java/com/investment/alphavantage/company/CompanyOverviewController.java
@@ -0,0 +1,45 @@
+package com.investment.alphavantage.company;
+
+import com.google.gson.Gson;
+import com.investment.alphavantageapi.model.company.CompanyOverviewData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class CompanyOverviewController {
+
+    private RestTemplate restTemplate;
+    private String apiKey;
+
+    @Autowired
+    public CompanyOverviewController(RestTemplate restTemplate, @Value("${alphavantage.api.key}") String apiKey) {
+        this.restTemplate = restTemplate;
+        this.apiKey = apiKey;
+    }
+
+    @GetMapping(value = "/test")
+    public CompanyOverviewData retrieveCompanyOverviewDataFor(String ticker) {
+        if (ticker == null || ticker.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        ResponseEntity<String> apiResponse =
+                restTemplate.getForEntity("https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=" + apiKey,
+                String.class);
+
+        CompanyOverviewData companyOverview = new Gson().fromJson(apiResponse.getBody(), CompanyOverviewData.class);
+
+        return CompanyOverviewData.builder()
+                .symbol(companyOverview.getSymbol())
+                .name(companyOverview.getName())
+                .description(companyOverview.getDescription())
+                .exchange(companyOverview.getExchange())
+                .sector(companyOverview.getSector())
+                .peRatio(companyOverview.getPeRatio())
+                .build();
+    }
+}
---
+++ src/test/java/com/investment/alphavantage/company/CompanyOverviewControllerTest.java
@@ -0,0 +1,108 @@
+package com.investment.alphavantage.company;
+
+import com.investment.alphavantageapi.model.company.CompanyOverviewData;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CompanyOverviewControllerTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private CompanyOverviewController companyOverviewController =
+            new CompanyOverviewController(restTemplate, "ALPHAVANTAGE_API_KEY");
+
+    @Test
+    public void shouldReturnCompanyOverviewDataObject() {
+        // given
+        String ticker = "IBM";
+
+        // when
+        CompanyOverviewData result = companyOverviewController.retrieveCompanyOverviewDataFor(ticker);
+
+        // then
+        assertNotNull(result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenTickerIsNull() {
+        // given
+        String ticker = null;
+
+        // when
+        CompanyOverviewData result = companyOverviewController.retrieveCompanyOverviewDataFor(ticker);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionWhenTickerIsEmpty() {
+        // given
+        String ticker = "";
+
+        // when
+        CompanyOverviewData result = companyOverviewController.retrieveCompanyOverviewDataFor(ticker);
+    }
+
+    @Test
+    public void shouldMakeCompanyOverviewRestTemplateCall() {
+        // given
+        String ticker = "IBM";
+
+        // when
+        companyOverviewController.retrieveCompanyOverviewDataFor(ticker);
+
+        // then
+        verify(restTemplate).getForEntity("https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=ALPHAVANTAGE_API_KEY",
+                String.class);
+    }
+
+    @Test
+    public void shouldReturnCompanyOverviewDetails() {
+        // given
+        String ticker = "IBM";
+
+        String name = "International Business Machines Corporation";
+        String description = "Blah Blah Blah";
+        String exchange = "NYSE";
+        String peRatio = "22.08";
+        String sector = "Technology";
+        String apiResponse = "{\n" +
+                "    \"Symbol\": \"" + ticker + "\",\n" +
+                "    \"Name\": \"" + name + "\",\n" +
+                "    \"Description\": \"" + description + "\",\n" +
+                "    \"Exchange\": \"" + exchange + "\",\n" +
+                "    \"Sector\": \"" + sector + "\",\n" +
+                "    \"PERatio\": \"" + peRatio + "\"\n" +
+                "}";
+
+        given(restTemplate.getForEntity("https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=ALPHAVANTAGE_API_KEY",
+                String.class)).willReturn(ResponseEntity.of(Optional.of(apiResponse)));
+
+        // when
+        CompanyOverviewData result = companyOverviewController.retrieveCompanyOverviewDataFor(ticker);
+
+        // then
+        assertEquals(ticker, result.getSymbol());
+        assertEquals(name, result.getName());
+        assertEquals(description, result.getDescription());
+        assertEquals(sector, result.getSector());
+        assertEquals(exchange, result.getExchange());
+        assertEquals(peRatio, result.getPeRatio());
+    }
+
+}
